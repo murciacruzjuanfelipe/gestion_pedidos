b28c345cd4f2938fe09681da47e99d1ac190d4d0
Juan felipe murcia cruz <murciacruzjuanfelipe@gmail.com>
2026-09-04 07:35:45 -0500

    primer commit del proyecto ejercicio_git

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$usuario = "root";
+$password = "";        // por defecto en XAMPP no tiene contraseña
+$basedatos = "gestion_pedidos";
+$puerto = 3306;
+
+$conexion = new mysqli($host, $usuario, $password, $basedatos, $puerto);
+
+if ($conexion->connect_error) {
+    die("Error de conexion: " . $conexion->connect_error);
+}
+
+echo "Conexion exitosa a " . $basedatos;
+$conexion->close();
