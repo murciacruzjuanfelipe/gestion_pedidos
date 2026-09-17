4608137540518171f29c1aba2f87a749f9997622
juan felipe murcia cruz <murciacruzjuanfelipe@gmail.com>
2026-09-17 10:32:34 -0500

    cambiar datos

--- conexion.php
+++ conexion.php
@@ -11,5 +11,5 @@ if ($conexion->connect_error) {
     die("Error de conexion: " . $conexion->connect_error);
 }
 
-echo "Conexion 2026 " . $basedatos;
+echo "Conexion  exitosa  " . $basedatos;
 $conexion->close();
